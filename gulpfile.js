2ce1dbae0b6fde30a2863303427ce1555cf2f838
unknown <lucas.gabule@gmail.com>
2018-05-02 09:55:22 +0800

    User Able to persist data

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,20 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
+elixir(function (mix) {
     mix.sass('app.scss');
+    styles([
+        'libs/blog-post.css',
+        'libs/bootstrap.css',
+        'libs/font-awesome.css',
+        'libs/metisMenu.css',
+        'libs/sb-admin-2.css',
+        'libs/styles.css'], './public/css/libs.js')
+        .scripts([
+            'libs/jquery.js',
+            'libs/bootstrap.js',
+            'libs/metisMenu.js',
+            'libs/sb-admin-2.js',
+            'libs/scripts.js'
+        ], './public/js/libs.js');
 });
--- resources/views/admin/Users/create.blade.php
+++ resources/views/admin/Users/create.blade.php
@@ -1,7 +1,44 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Black_Programmer
- * Date: 5/1/2018
- * Time: 7:15 PM
- */
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Create New Users</h1>
+    {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store', 'files'=> true]) !!}
+    <div class="form-group">
+        {!! Form::label('name', 'Name:') !!}
+        {!! Form::text('name', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('email', 'Email:') !!}
+        {!! Form::text('email', null, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('password', 'Password:') !!}
+        {!! Form::password('password', ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('role_id', 'Role:') !!}
+        {!! Form::select('role_id', [''=> 'Choose Options'] + $roles, null, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('is_active', 'Status:') !!}
+        {!! Form::select('is_active', array(1 => 'Active', 0 => 'Not Active'), 0, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('file', 'File:') !!}
+        {!! Form::file('file', null, ['class'=>'form-control']) !!}
+    </div>
+
+
+    <div class="form-group">
+        {!! Form::submit('Create User', ['class'=>'btn btn-default']) !!}
+    </div>
+    {!! Form::close() !!}
+
+    @include('includes.form_errors')
+
+@stop
